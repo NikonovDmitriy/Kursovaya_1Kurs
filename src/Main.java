ed2c861ea81508732b8b612219e9657986bbe081
nikonovdmitiy <nikonov9094@gmail.com>
2022-09-10 20:07:04 +0500

    Kursovaya_1Kurs

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,28 @@
 public class Main {
     private static Employee[] employees = new Employee[10];
 
+    public static void main(String[] args) {
+        employees[0] = new Employee("Иванов Иван Иванович", 1, Math.ceil(Math.random() * 2000));
+        employees[1] = new Employee("Семенов Семен Семенович", 2, Math.ceil(Math.random() * 2000));
+        employees[2] = new Employee("Федоров Федор Федорович", 5, Math.ceil(Math.random() * 2000));
+        employees[3] = new Employee("Дмитриев Дмитрий Дмитриевич", 3, Math.ceil(Math.random() * 2000));
+        employees[4] = new Employee("Васильев Василий Васильевич", 4, Math.ceil(Math.random() * 2000));
+        employees[5] = new Employee("Петров Петр Петрович", 5, Math.ceil(Math.random() * 2000));
+        employees[6] = new Employee("Андреев Андрей Андреевич", 1, Math.ceil(Math.random() * 2000));
+        employees[7] = new Employee("Константинов Константин Константинович", 2, Math.ceil(Math.random() * 2000));
+        employees[8] = new Employee("Витальев Виталий Витальевич", 3, Math.ceil(Math.random() * 2000));
+        employees[9] = new Employee("Евгенин Евгений Евгеньевич", 4, Math.ceil(Math.random() * 2000));
+
+
+        System.out.println();
+        System.out.println("Сумма затрат на зарплату сотрудников в месяц " + getSum() + " руб.");
+        getMinPay();
+        getMaxPay();
+        System.out.println("Среднее значение зарплат сотрудников " + averageSalary() + " руб.");
+        System.out.println();
+
+    }
+
 
     private static double getSum() {
         double sum = 0;
@@ -47,35 +69,10 @@ public class Main {
     }
 
 
-
     public static void printEmployees() {
         for (Employee e : employees) {
             System.out.println(e.toString());
         }
-    }
-
-
-
-    public static void main(String[] args) {
-        employees[0] = new Employee("Иванов Иван Иванович", 1, Math.ceil(Math.random() * 2000));
-        employees[1] = new Employee("Семенов Семен Семенович", 2, Math.ceil(Math.random() * 2000));
-        employees[2] = new Employee("Федоров Федор Федорович", 5, Math.ceil(Math.random() * 2000));
-        employees[3] = new Employee("Дмитриев Дмитрий Дмитриевич", 3, Math.ceil(Math.random() * 2000));
-        employees[4] = new Employee("Васильев Василий Васильевич", 4, Math.ceil(Math.random() * 2000));
-        employees[5] = new Employee("Петров Петр Петрович", 5, Math.ceil(Math.random() * 2000));
-        employees[6] = new Employee("Андреев Андрей Андреевич", 1, Math.ceil(Math.random() * 2000));
-        employees[7] = new Employee("Константинов Константин Константинович", 2, Math.ceil(Math.random() * 2000));
-        employees[8] = new Employee("Витальев Виталий Витальевич", 3, Math.ceil(Math.random() * 2000));
-        employees[9] = new Employee("Евгенин Евгений Евгеньевич", 4, Math.ceil(Math.random() * 2000));
-
-
-        System.out.println();
-        System.out.println("Сумма затрат на зарплату сотрудников в месяц " + getSum() + " руб.");
-        getMinPay();
-        getMaxPay();
-        System.out.println("Среднее значение зарплат сотрудников " + averageSalary() + " руб.");
-        System.out.println();
-
     }
 }
 
